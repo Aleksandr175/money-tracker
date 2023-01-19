89c2b23d74ef8a932fe932c90ea42e2e7c4c65aa
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2023-01-18 22:53:27 -0300

    Change categories

--- resources/js/Pages/Categories.tsx
+++ resources/js/Pages/Categories.tsx
@@ -41,18 +41,22 @@ export default function Categories(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">Categories</div>
+
+                        {isLoading ? (
+                            <div>Loading...</div>
+                        ) : (
+                            <div>
+                                {categories.map((category) => {
+                                    return (
+                                        <div key={category.id}>
+                                            {category.name}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        )}
                     </div>
                 </div>
-
-                {isLoading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <div>
-                        {categories.map((category) => {
-                            return <div>{category.name}</div>;
-                        })}
-                    </div>
-                )}
             </div>
         </AuthenticatedLayout>
     );
